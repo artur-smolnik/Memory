d7cadef2fcf5020cf5d810190691e322eb5803ed
Student 249092 <249092@student.pwr.edu.pl>
2019-05-05 16:35:31 +0200

    numbers works properly

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
 
             var elem = document.getElementById('canvas');           
-            var my_context = canvas.getContext('2d');
-            var myRect = [];
+            var myCanvas = canvas.getContext('2d');
+            var grid = [];
             var numbersToGuess = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
             var columns = 4;
             var rows = 4;
+            var shapeSize = 50;
+            var pressedRectNumber1 = 0;
+            var pressedRectNumber2 = 0;
+
+            myCanvas.font = "20px Comic Sans MS";
             function shuffle(array) {
                 array.sort(() => Math.random() - 0.5);
               }
@@ -15,97 +20,119 @@
                 console.log('canvas click');
              });
            
-            elem.addEventListener('click', on_canvas_click, false);
+            elem.addEventListener('click', canvasOnClick, false);
             
             
-            function on_canvas_click(event) {
+            function canvasOnClick(event) {
                  offsetX = event.clientX - elem.offsetLeft;
                  offsetY = event.clientY - elem.offsetTop;
                 
-                for (var i in myRect) {
-                    oRec = myRect[i];
+                for (var i in grid) {
+                    rectangle = grid[i];
                     
-                    if(offsetX >= oRec.x && offsetX <=oRec.x+oRec.w && offsetY >= oRec.y && offsetY <= oRec.y+oRec.h){
-                        console.log([oRec.x, oRec.w]);
-                        myRect[i]= new Shape(oRec.x, oRec.y, 20, 20, "white");
-                        for (var i in myRect) {
-                            oRec = myRect[i];
-                            my_context.fillStyle = oRec.deafaultColor;
-                            my_context.fillRect(oRec.x, oRec.y, oRec.w, oRec.h);
+                    if(offsetX >= rectangle.x && offsetX <=rectangle.x+rectangle.w && offsetY >= rectangle.y && offsetY <= rectangle.y+rectangle.h){                                             
+                       
+                        myCanvas.fillStyle = "white";
+                        myCanvas.fillRect(rectangle.x, rectangle.y, rectangle.w, rectangle.h);           
+                        // for (var y in grid) {
+                            
+                        //     rectangle = grid[y];
+                        //     myCanvas.fillStyle = rectangle.deafaultColor;
+                        //     myCanvas.fillRect(rectangle.x, rectangle.y, rectangle.w, rectangle.h);                         
                     
+                        // }
+                        if(pressedRectNumber1 == 0 && pressedRectNumber2 == 0 ) 
+                        {
+                            pressedRectNumber1 = rectangle.numberToGuess;
                         }
-                document.getElementById("button").src = "Images/jerry.jfif";
+                        else if (pressedRectNumber1 != 0 && pressedRectNumber2 == 0)
+                        {
+                            pressedRectNumber2 = rectangle.numberToGuess;
+                        }
+                        else if (pressedRectNumber1 !=0 && pressedRectNumber2 != 0)
+                        {
+                            myCanvas.fillStyle = "green";
+                            myCanvas.fillRect(400, 180, 70,70);
+                            pressedRectNumber1 = rectangle.numberToGuess;
+                            pressedRectNumber2 = 0;
+                        }
+                        printNumber();
+                        //myCanvas.strokeText(rectangle.numberToGuess, rectangle.x, rectangle.y);
 
                     }     
                 }                
             }
             
             
-            function Shape(x, y, w, h, deafaultColor, numberToGuess) {
+            function printNumber(){
+                myCanvas.fillStyle = "blue";
+                if(pressedRectNumber1 != 0) myCanvas.fillText(pressedRectNumber1, 400, 200);
+                if(pressedRectNumber2 != 0) myCanvas.fillText(pressedRectNumber2, 400, 250);
+               
+            }
+
+       
+
+            function Shape(x, y, w, h, deafaultColor, numberToGuess, uncoveredColor) {
                 this.x = x;
                 this.y = y;
                 this.w = w;
                 this.h = h;
                 this.deafaultColor = deafaultColor;
                 this.numberToGuess = numberToGuess;
+                this.uncoveredColor = uncoveredColor;
             }
-            
-            
+                 
+           
+           
+           
+           
             var xvalue = 0;
             var yvalue = 0;
-            
-            function RollNumbersToGuess(){
-                do{
-
-                }while(numbersToGuess.length<20)
-            }
-   
-            
-            for (var x = 0; x < columns*rows; x++) {
-                 //my_context.fillRect(xvalue, yvalue, 20, 20); 
-                xvalue += 25;
+            for (var x = 0; x < columns*rows; x++) {                 
+                xvalue += shapeSize+5;
                 
                 if ((x) % columns === 0) {
-                    yvalue += 25;
+                    yvalue += shapeSize+5;
                     xvalue = 0;
                 }    
-                myRect.push(new Shape(xvalue, yvalue, 20, 20, "red", numbersToGuess[x]));
+                grid.push(new Shape(xvalue, yvalue, shapeSize, shapeSize, "red", numbersToGuess[x]), "black");
             }              
-                for (var i in myRect) {
-                    oRec = myRect[i];
-                    my_context.fillStyle = oRec.deafaultColor;
-                    my_context.fillRect(oRec.x, oRec.y, oRec.w, oRec.h);
+            for (var i in grid) {
+                rectangle = grid[i];
+                myCanvas.fillStyle = rectangle.deafaultColor;
+                myCanvas.fillRect(rectangle.x, rectangle.y, rectangle.w, rectangle.h);
             
-                }
+            }
             
-                document.body.onclick = function(e) {
-                    e = e || window.event;
+                // document.body.onclick = function(e) {
+                //     e = e || window.event;
                   
-                    var target = e.target || e.srcElement,
-                      rect = target.getBoundingClientRect(),
-                      offsetX = e.clientX - rect.left,
-                      offsetY = e.clientY - rect.top;
+                //     var target = e.target || e.srcElement,
+                //       rect = target.getBoundingClientRect(),
+                //       offsetX = e.clientX - rect.left,
+                //       offsetY = e.clientY - rect.top;
                   
-                    //console.log([offsetX, offsetY]);
-                  };
+                //     //console.log([offsetX, offsetY]);
+                //   };
 
 
                  
                  
             //       or (var x = 0; x < 100; x++) {
-            //         //my_context.fillRect(xvalue, yvalue, 20, 20); 
+            //         //myCanvas.fillRect(xvalue, yvalue, 20, 20); 
             //        xvalue += 25;
                    
             //        if ((x) % 10 === 0) {
             //            yvalue += 25;
             //            xvalue = 0;
             //        }    
-            //        myRect.push(new Shape(xvalue, yvalue, 20, 20, "red"));
+            //        grid.push(new Shape(xvalue, yvalue, 20, 20, "red"));
             //    }              
-            //        for (var i in myRect) {
-            //            oRec = myRect[i];
-            //            my_context.fillStyle = oRec.deafaultColor;
-            //            my_context.fillRect(oRec.x, oRec.y, oRec.w, oRec.h);
+            //        for (var i in grid) {
+            //            rectangle = grid[i];
+            //            myCanvas.fillStyle = rectangle.deafaultColor;
+            //            myCanvas.fillRect(rectangle.x, rectangle.y, rectangle.w, rectangle.h);
                
             //        }
                
@@ -136,7 +163,7 @@
 
             // for (var x = 0; x <= 10; x++) {
             //     for (var y = 0; y <= 10; y++) {
-            //         my_context.fillRect(x*40, y*40, 20, 20);
+            //         myCanvas.fillRect(x*40, y*40, 20, 20);
             //     }
             // }
             // // Resetowanie czasów reakcji
